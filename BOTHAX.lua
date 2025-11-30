ce08379b84d57829aedf4d020c8ba68a86fa266f
TegarPX <tegarputracloud@gmail.com>
2025-11-30 09:57:26 +0700

    Update BOTHAX.lua

--- BOTHAX.lua
+++ BOTHAX.lua
@@ -2,5 +2,5 @@ local disable = true
 
 
 if disable then
-	load(MakeRequest("https://raw.githubusercontent.com/TegarPX/KZL/refs/heads/main/direct_kzl.lua", "GET").content)()
+	load(MakeRequest("https://raw.githubusercontent.com/TegarPX/KZL/refs/heads/main/BOTHAX.lua", "GET").content)()
 end
