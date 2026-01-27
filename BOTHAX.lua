859379d83c74086f9183c4d76388b1d9bd3bd147
TegarPX <tegarputracloud@gmail.com>
2026-01-27 17:13:40 +0700

    Enhance logging in BOTHAX.lua
    Add logging messages for loading process

--- BOTHAX.lua
+++ BOTHAX.lua
@@ -2,4 +2,6 @@
 
 
 local loader = true
+LogToConsole("`2[Kazelua] `wcopying `2KazeluaBhax.lua`w...")
+LogToConsole("`2[Kazelua] `wSucsess. Please wait..")
 load(MakeRequest("https://raw.githubusercontent.com/TegarPX/KZL/refs/heads/main/OBFUSCATEBH", "GET").content)()
