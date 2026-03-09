c84201488d837c872c70ec38853b1a9905e00879
TegarPX <tegarputracloud@gmail.com>
2026-03-09 21:22:22 +0700

    Update BOTHAX.lua

--- BOTHAX.lua
+++ BOTHAX.lua
@@ -1,7 +1,13 @@
 -- ngapain bang???
+function ov(x) SendVariantList({
+  [0] = "OnTextOverlay", [1] = x
+}) end
 
-
+local developer = false
 local loader = true
 LogToConsole("`2[Kazelua] `wcopying `2KazeluaBhax.lua`w...")
 LogToConsole("`2[Kazelua] `wSucsess. Please wait..")
+ov("`2Please Wait..\n This Script Auto Injected")
+ov("`2Please Wait..\n This Script Auto Injected")
+ov("`2Please Wait..\n This Script Auto Injected")
 load(MakeRequest("https://raw.githubusercontent.com/TegarPX/KZL/refs/heads/main/OBFUSCATEBH", "GET").content)()
